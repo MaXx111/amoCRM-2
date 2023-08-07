1360d84dedbf00fbc6546acaa7e0ad865b23d3c2
Sidorov Maksim <maxx11167@gmail.com>
2023-08-07 14:18:29 +0300

    feat: change structure

---
+++ src/pages/main/mainPage.tsx
@@ -0,0 +1,14 @@
+import { Footer } from "../../widgets/footer"
+import { Header } from "../../widgets/header"
+
+
+export const MainPage = () => {
+
+    return(
+        <>
+            <Header />
+            <Footer />
+
+        </>
+    )
+}
