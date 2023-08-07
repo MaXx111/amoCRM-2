232e1a888fa84c7f412196cbe70aaec96b125d62
Sidorov Maksim <maxx11167@gmail.com>
2023-08-07 22:26:32 +0300

    fix links in index.html

--- src/pages/main/lib/customize.tsx
+++ src/pages/main/lib/customize.tsx
@@ -12,13 +12,13 @@ export const Customize = () => {
                     <p className="customize-text orbitron-regular">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae.</p>
                     <div className="customize-imgs">
                         <a href="!#" className="customize-img-link">
-                            <img src="../../public/customize-img-1.png" alt="customize-img-1" className="customize-img"/>
+                            <img src="./customize-img-1.png" alt="customize-img-1" className="customize-img"/>
                         </a>
                         <a href="!#" className="customize-img-link">
-                            <img src="../../public/customize-img-2.png" alt="customize-img-2" className="customize-img"/>
+                            <img src="./customize-img-2.png" alt="customize-img-2" className="customize-img"/>
                         </a>
                         <a href="!#" className="customize-img-link">
-                            <img src="../../public/customize-img-3.png" alt="customize-img-3" className="customize-img"/>
+                            <img src="./customize-img-3.png" alt="customize-img-3" className="customize-img"/>
                         </a>
                     </div>
                 </div>
--- src/pages/main/lib/news.tsx
+++ src/pages/main/lib/news.tsx
@@ -17,28 +17,28 @@ export const News = () => {
                 </div>
                 <div className="news-content">
                     <article className="news-item">
-                        <img src="../../public/news-img-1.png" alt="" className="news-item-img" />
+                        <img src="./news-img-1.png" alt="" className="news-item-img" />
                         <p className="news-item-date orbitron-regular">April 2, 2021</p>
                         <h3 className="news-item-title orbitron-regular">Esports Group teams up with the Indianapolis Colts</h3>
                     </article>
                     <div className="news-little-articles">
                         <article className="news-item">
-                            <img src="../../public/news-img-2.png" alt="" className="news-item-img"/>
+                            <img src="./news-img-2.png" alt="" className="news-item-img"/>
                             <p className="news-item-date orbitron-regular">April 2, 2021</p>
                             <h3 className="news-item-title orbitron-regular">NAVI reveals s1mple fifth anniversary</h3>
                         </article>
                         <article className="news-item">
-                            <img src="../../public/news-img-3.png" alt="" className="news-item-img"/>
+                            <img src="./public/news-img-3.png" alt="" className="news-item-img"/>
                             <p className="news-item-date orbitron-regular">April 2, 2021</p>
                             <h3 className="news-item-title orbitron-regular">A1esports Shares new picture</h3>
                         </article>
                         <article className="news-item">
-                            <img src="../../public/news-img-4.png" alt="" className="news-item-img"/>
+                            <img src="./public/news-img-4.png" alt="" className="news-item-img"/>
                             <p className="news-item-date orbitron-regular">April 2, 2021</p>
                             <h3 className="news-item-title orbitron-regular">T1 unveil partnership with Razers</h3>
                         </article>
                         <article className="news-item">
-                            <img src="../../public/news-img-5.png" alt="" className="news-item-img"/>
+                            <img src="./public/news-img-5.png" alt="" className="news-item-img"/>
                             <p className="news-item-date orbitron-regular">April 2, 2021</p>
                             <h3 className="news-item-title orbitron-regular">RX secures content partnership with</h3>
                         </article>
